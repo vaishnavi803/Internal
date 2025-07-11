c3c2a1541d4f6bd88ab852ff6153b5fdadc99feb
vaishnavi803 <ashdevatha7@gmail.com>
2025-07-11 12:31:27 +0530

    Add files via upload

---
+++ Sumavg.java
@@ -0,0 +1,14 @@
+public class Sumavg{
+ public static void main(String args[]){
+  int n=10;
+  int su=0;
+  int avg=0;
+  for(int i=0;i<=n;i++){
+   su+=i;
+   }
+   avg=su/n;
+   System.out.println("Sum of 10 nums:"+su);
+   System.out.println("Avg of 10 nums:"+avg);
+   }
+   }
+   
